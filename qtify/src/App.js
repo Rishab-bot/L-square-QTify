c934734dc80821bd1ca6731c56bae3140c49a970
unknown <prataplavi@gmail.com>
2024-12-30 22:39:41 +0530

    changes made

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar.jsx';
-import Hero from './Components/Hero/Hero.jsx';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   return (
