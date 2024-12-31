df19e32040ff1032185ae11c88bb8f6da5b005d7
unknown <prataplavi@gmail.com>
2024-12-31 22:36:00 +0530

    changes made

--- qtify/src/Components/FilterSection/FilterSection.jsx
+++ qtify/src/Components/FilterSection/FilterSection.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import styles from "./FilterSection.module.css";
 import Section from '../Section/Section.jsx';
 import BasicTabs from '../BasicTabs/BasicTabs';
-import Carousel from '../Carousel/Carousel';
-import Card from '../../Card/Card';
+import Carousel from '../Carousel/Carousel.jsx';
+import Card from '../Card/Card.jsx';
 import { CircularProgress } from '@mui/material';
 
 const FilterSection = ({type, title, value, filteredData, handleChangeIndex}) => {
